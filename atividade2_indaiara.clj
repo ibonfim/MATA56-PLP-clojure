0d06a1dfc93a152fe417bde421dfbb9a1329530b
mendes7 <natanmendesramos@gmail.com>
2025-02-04 22:46:39 -0300

    Ajustes nas funções principais

--- atividade2_indaiara.clj
+++ atividade2_indaiara.clj
@@ -1,45 +1,45 @@
-(ns datashow)
+(ns datashow-reservas.core
+  (:require [clojure.string :as str]))
 
 ;; Estrutura inicial com a lista de datashows
 (def datashows
-  [{:id 1 :status :disponivel}
-   {:id 2 :status :disponivel}
-   {:id 3 :status :alocado}
-   {:id 4 :status :disponivel}])
+  (atom [{:id 1 :status :disponivel}
+         {:id 2 :status :disponivel}
+         {:id 3 :status :alocado}
+         {:id 4 :status :disponivel}]))
+
+;; Banco de dados de reservas
+(def reservas (atom {}))
 
 ;; Função para consultar o status de um datashow
-(defn consultar [datashows id]
-  (loop [ds datashows]
-    (cond
-      (empty? ds) nil
-      (= (:id (first ds)) id) (:status (first ds))
-      :else (recur (rest ds)))))
+(defn consultar [id]
+  (some #(when (= (:id %) id) (:status %)) @datashows))
+
+;; Função para atualizar o status de um datashow
+(defn atualizar-status [id novo-status]
+  (swap! datashows #(mapv (fn [ds] (if (= (:id ds) id) (assoc ds :status novo-status) ds)) %)))
 
 ;; Função para alocar um datashow
-(defn alocar [datashows id]
-  (loop [ds datashows]
-    (cond
-      (empty? ds) (str "Datashow " id " não encontrado.")
-      (= (:id (first ds)) id)
-      (if (= (:status (first ds)) :disponivel)
-        (cons (assoc (first ds) :status :alocado) (rest ds))  ;; Atualiza o status do datashow
-        (str "Datashow " id " já está alocado."))
-      :else (recur (rest ds)))))
+(defn alocar [id]
+  (if-let [ds (some #(when (= (:id %) id) %) @datashows)]
+    (if (= (:status ds) :disponivel)
+      (do (atualizar-status id :alocado)
+          (println "Datashow alocado com sucesso!"))
+      (println "Datashow já está alocado."))
+    (println "Datashow não encontrado.")))
 
 ;; Função para desalocar um datashow
-(defn desalocar [datashows id]
-  (loop [ds datashows]
-    (cond
-      (empty? ds) (str "Datashow " id " não encontrado.")
-      (= (:id (first ds)) id)
-      (if (= (:status (first ds)) :alocado)
-        (cons (assoc (first ds) :status :disponivel) (rest ds))  ;; Atualiza o status do ds
-        (str "Datashow " id " não estava alocado."))
-      :else (recur (rest ds)))))
+(defn desalocar [id]
+  (if-let [ds (some #(when (= (:id %) id) %) @datashows)]
+    (if (= (:status ds) :alocado)
+      (do (atualizar-status id :disponivel)
+          (println "Datashow desalocado com sucesso!"))
+      (println "Datashow não estava alocado."))
+    (println "Datashow não encontrado.")))
 
-;; Função auxiliar para exibir o estado atual da lista de datashows
-(defn exibir-datashows [datashows]
-  (doseq [ds datashows]
+;; Lista todos os datashows
+(defn exibir-datashows [] 
+  (doseq [ds @datashows]
     (println "ID:" (:id ds) "Status:" (:status ds))))
 
 ;; Exemplos de uso
